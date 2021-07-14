ebd41f95b12148dd8b906b7aa6e93c475b7aa1d6
oukali <oukalisalim@outlook.com>
2021-07-14 10:34:27 -0400

    Version1

---
+++ script10.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# script2.sh
+# i prend chacune des valeurs possibles correspondant à l’occurrence
+#read -p "Entrer une xtension : " reponse
+for i in {1..10}
+do
+	echo "Valeur de i : ${i}"
+#	exit 0
+	break
+done
+echo "Execution du reste des instructions"
+
